5149454db7ac331698cc73530e7d928a424ac55e
Micaelly Mariano <148275449+MicaellyMariano@users.noreply.github.com>
2025-05-31 17:03:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -148,3 +148,16 @@ function toggleContent(contentId) {
     button.textContent = "Leia mais";
   }
 }
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = this.getAttribute('href').substring(1);
+      const target = document.getElementById(id);
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
+
